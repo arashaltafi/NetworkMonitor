e62daf99bc4d1e31d56c1b0992bf247533ef0e65
Arash Altafi <arashaltafi1377@gmail.com>
2022-09-25 21:59:40 +0330

    Initial commit

---
+++ app/src/main/java/com/arashaltafi/speedtest/AutoStartup.kt
@@ -0,0 +1,17 @@
+package com.arashaltafi.speedtest
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AutoStartup : BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        val sharedPreferences = p0?.getSharedPreferences("serviceCheck", Context.MODE_PRIVATE)
+        if (sharedPreferences!!.getBoolean("serviceRunning", false)) {
+            val intent = Intent(p0, InternetSpeedMeter::class.java)
+            p0.startService(intent)
+        }
+    }
+}
